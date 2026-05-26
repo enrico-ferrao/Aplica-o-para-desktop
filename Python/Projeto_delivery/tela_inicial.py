200ffc3d0896a3c383ba9ce98169010a4ce8a077
Enrico Ferrao <enricogferrao@gmail.com>
2026-05-26 20:13:48 -0300

    Create tela_inicial.py

---
+++ Python/Projeto_delivery/tela_inicial.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+import subprocess
+import sys
+
+#funçoes
+def tela_T():
+    janela.destroy()
+
+
+def tela_P():
+    janela.destroy()
+
+def tela_S():
+    subprocess.Popen([sys.executable,"Projeto_delivery/tela_login.py"])
+    janela.destroy()
+
+#janela
+janela = tk.Tk()
+
+janela.title("Tela inicial")
+janela.geometry("800x400")
+
+#titulo central
+label_titulo_central = tk.Label(   
+    janela,
+    text = "Escolha a tela",
+    font = (
+        "Arial",
+        30
+            ))
+label_titulo_central.pack()
+
+#botao trabalhador
+
+botaoT = tk.Button(
+    janela,
+    text = "Trabalhador",
+    command = tela_T,
+    width = 20,
+    height = 3
+)
+botaoT.pack(pady = 20)
+
+
+#botao pedido
+
+botaoP = tk.Button(
+    janela,
+    text = "Pedido",
+    command = tela_P,
+    width = 20,
+    height = 3
+)
+botaoP.pack(pady=20)
+
+#botao sair
+botaoS = tk.Button(
+    janela,
+    text = "Logout",
+    command = tela_S,
+    width = 20, 
+    height = 3
+)
+botaoS.pack(pady=20)
+#inciar
+janela.mainloop()
