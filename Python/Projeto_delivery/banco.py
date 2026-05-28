2800a40340301dca1c806ddb188514b14335e752
Enrico Ferrao <enricogferrao@gmail.com>
2026-05-28 04:12:58 -0300

    Update banco.py

--- Python/Projeto_delivery/banco.py
+++ Python/Projeto_delivery/banco.py
@@ -2,9 +2,10 @@ import mysql.connector
 
 #conectando mysql
 
-conexao = mysql.connector.connect (
-    host = "127.0.0.1",
-    user = "root",
-    password = "----",
-    database =  "delivery_db"
+conexao = mysql.connector.connect(
+    host = "deliverydb-enricogferrao-d95a.l.aivencloud.com",
+    port = 12884,
+    user = "avnadmin",
+    password = ".....",
+    database = "defaultdb"
 )
